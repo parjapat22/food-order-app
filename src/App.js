c8642881df68a7a917b94a51c139d2f0bc453e7f
Basant Lal <bsntparjapat8@gmail.com>
2022-07-11 16:19:00 +0100

    header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import React, { Fragment } from 'react';
+import Header from './components/Layout/Header';
+
 function App() {
   return (
-    <div>
-      <h1>Food Order App</h1>
-    </div>
+    <Fragment>
+      <Header />
+    </Fragment>
   );
 }
 
---
+++ src/components/Layout/Header.js
@@ -0,0 +1,23 @@
+import React, { Fragment } from 'react';
+import HeaderCartButton from './HeaderCartButton';
+import classes from './Header.module.css';
+import mealsImage from '../../assets/meals.jpg';
+
+const Header = (props) => {
+  return (
+    <Fragment>
+      <div className={classes.header}>
+        <header className={classes.headerButton}>
+          <h1>ReactMeals</h1>
+          <HeaderCartButton />
+        </header>
+      </div>
+
+      <div className={classes['main-image']}>
+        <img src={mealsImage} alt='A table full of delicious food!' />
+      </div>
+    </Fragment>
+  );
+}
+
+export default Header;
