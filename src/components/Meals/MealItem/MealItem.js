eb80c3dd3b1fd91d4a72a17d4df4b8b72a424d71
Basant Lal <bsntparjapat8@gmail.com>
2022-07-12 15:22:37 +0100

    item quantity

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import MealItemForm from './MealItemForm';
 import classes from './MealItem.module.css';
 
 const MealItem = (props) => {
@@ -11,6 +12,8 @@ const MealItem = (props) => {
         <div className={classes.description}>{props.description}</div>
         <div className={classes.price}>{price}</div>
       </div>
+
+      <div><MealItemForm id={props.id} /></div>
     </li>
   );
 }
